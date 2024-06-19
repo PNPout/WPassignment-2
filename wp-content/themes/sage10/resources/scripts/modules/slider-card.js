b302a052baf0a4c8cf311707d05246ce5dffe33b
PNPout <prakash.poudel@outside.tech>
2024-06-19 11:28:31 +0545

    Slider Module
    https://outsidetech.atlassian.net/browse/OAD-219

---
+++ wp-content/themes/sage10/resources/scripts/modules/slider-card.js
@@ -0,0 +1,18 @@
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const homeSwiper = new Swiper('.js-swiper', {
+  modules: [Navigation, Pagination],
+  speed: 400,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swipe-next',
+    prevEl: '.swipe-prev',
+  },
+});
+
+homeSwiper();
---
+++ wp-content/themes/sage10/resources/views/blocks/cards/slider-card.blade.php
@@ -0,0 +1,18 @@
+
+          <div class="slider-card swiper-slide">
+            <div class="slider-card-media">
+              <img
+                src="{{ $imgScr }}"
+                alt="Image of a lady" 
+                class="slider-card-media__image"
+              />
+            </div>
+            <div class="slider-card-content">
+              <h5 class="slider-card-content__title">
+               {{ $slideTitle}}
+              </h5>
+              <p class="slider-card-content__desc body-text__xl">
+              {{ $slideDesc}}
+              </p>
+            </div>
+          </div>
