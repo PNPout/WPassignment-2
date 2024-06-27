464dbfc4eba49abc0c5f2dc478969c8b3c5426e8
PNPout <prakash.poudel@outside.tech>
2024-06-27 09:07:51 +0545

    [OAD-228] header refinement done for tab & desktop

--- wp-content/themes/sage10/resources/views/sections/header.blade.php
+++ wp-content/themes/sage10/resources/views/sections/header.blade.php
@@ -11,18 +11,21 @@
 </header> -->
 
 <header class="header">
-  <div class="header__container container p-0 d-flex flex-row justify-content-between align-items-center">
-    <div class="header__wrapper px-6 py-4">
-      <div class="header-left d-flex gap-8 justify-content-start">
+  <div class="header__container container p-0">
+    <div class="header__wrapper px-6 py-4 px-lg-10 py-lg-8 py-md-6 d-flex flex-row justify-content-between align-items-center">
+      <div class="header-left d-flex gap-9 justify-content-start">
         <div class="header-left__navbar navbar d-md-flex d-lg-none">
           <button class="header-left__navbar-toggle navbar-toggler border border-0 p-0 bg-transparent" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="navbarOffcanvasLg" aria-label="Toggle navigation">
-            <span class="header-left__navbar-icon navbar-toggler-icon"></span>
+            <!-- <span class="header-left__navbar-icon navbar-toggler-icon"></span> -->
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 14H15M1 2H15H1ZM1 8H15H1Z" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
           </button>
         </div>
         <a href="{{ home_url('/') }}"><img src="@asset('./images/logo.svg')" alt="Logo of Followed Text" class="header-left__logo" /></a>
       </div>
       <div class="header-right">
-        <nav class="header-right-nav d-flex flex-lg-row flex-column  offcanvas-lg offcanvas-start bg-primary" id="offcanvasResponsive" role="navigation">
+        <nav class="header-right-nav d-flex flex-lg-row flex-column  offcanvas-lg offcanvas-start" id="offcanvasResponsive" role="navigation">
           <div class="header-offcanvas d-flex d-lg-none flex-row gap-6 px-6 py-9 align-items-center border border-peach">
             <button type="button" class="header-offcanvas__close btn-close btn-close-white d-flex d-lg-none p-0 border border-0 bg-transparent" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close offcanvas menu">
               <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -40,21 +43,21 @@
               <img src="@asset('./images/logo-white.svg')" alt="Logo of Followed Text" class="header-offcanvas__logo" />
             </a>
           </div>
-          <ul class="header-right-nav__list d-flex flex-lg-row flex-column px-10 py-11 h-100 align-items-center justify-content-center gap-10 list-unstyled">
+          <ul class="header-right-nav__list d-flex flex-lg-row flex-column px-10 py-11 px-lg-0 h-100 align-items-center justify-content-center gap-10 list-unstyled my-0">
             <li class="header-right-nav__item h6 mb-0 text-capitalize">
-              <a href="#" class="header-right-nav__link text-white " aria-label="Read about our company">About</a>
+              <a href="#" class="header-right-nav__link" aria-label="Read about our company">About</a>
             </li>
             <li class="header-right-nav__item h6 mb-0 text-capitalize">
-              <a href="#" class="header-right-nav__link text-white " aria-label="Read our blog">Blog</a>
+              <a href="#" class="header-right-nav__link" aria-label="Read our blog">Blog</a>
             </li>
             <li class="header-right-nav__item h6 mb-0 text-capitalize">
-              <a href="#" class="header-right-nav__link text-white" aria-label="Know more about our team">Team</a>
+              <a href="#" class="header-right-nav__link" aria-label="Know more about our team">Team</a>
             </li>
             <li class="header-right-nav__item h6 mb-0 text-capitalize">
-              <a href="#" class="header-right-nav__link text-white" aria-label="Explore career with us">Career</a>
+              <a href="#" class="header-right-nav__link" aria-label="Explore career with us">Career</a>
             </li>
             <li class="header-right-nav__item h6 mb-0 text-capitalize">
-              <a href="#" class="header-right-nav__link text-white" aria-label="Contact us">Contact</a>
+              <a href="#" class="header-right-nav__link" aria-label="Contact us">Contact</a>
             </li>
           </ul>
         </nav>
