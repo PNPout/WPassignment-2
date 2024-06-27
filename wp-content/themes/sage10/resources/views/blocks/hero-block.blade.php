f6c7fd3c2eec7b4427f0a454c11f122e3b2ff435
Prakash Poudel <prakash.poudel@outside.tech>
2024-06-27 15:43:17 +0545

    Hero section refinement for desktop

--- wp-content/themes/sage10/resources/views/blocks/hero-block.blade.php
+++ wp-content/themes/sage10/resources/views/blocks/hero-block.blade.php
@@ -23,22 +23,22 @@ EnqueueAssetsJS:
 
 <section class="hero-block">
     <div class="container p-0">
-        <div class="py-8 px-6 p-md-10 p-lg-8">
-            <div class="row g-6 g-md-10">
-                <div class="col-md-5 col-12 pe-md-0">
+        <div class="py-8 px-6 p-md-10 p-lg-20">
+            <div class="row g-6 gy-lg-16 g-md-10">
+                <div class="col-md-5 col-12 pe-md-0 pe-lg-10">
                     <img src="@asset('./images/hero-2.jpg')" alt="hero 1" class="hero-block__img hero-block__img--first">
                 </div>
-                <div class="col-7 ps-md-10 d-none d-md-flex">
+                <div class="col-7 ps-md-10 ps-lg-4 d-none d-md-flex">
                     <img src="@asset('./images/hero-1.jpg')" alt="hero 2" class="hero-block__img hero-block__img--second d-md-none d-lg-flex">
                     <img src="@asset('./images/hero-1-tab.jpg')" alt="hero 2 tab" class="hero-block__img hero-block__img--second d-md-flex d-lg-none">
                 </div>
                 <div class="col-lg-8 d-none d-md-flex">
-                    <div class="hero-block__content bg-white p-6 p-lg-8 border-success">
+                    <div class="hero-block__content bg-white p-md-10 p-lg-20 border-success">
                         <p class="h2">Look deep into nature, and then you will understand everything better.</p>
-                        <p class="body-xl">Contrary to popular belief, Lorem Ipsum is not simply random text. It has
+                        <p class="body-text__xl">Contrary to popular belief, Lorem Ipsum is not simply random text. It has
                             roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
                         </p>
-                        <a href="#" class="button button__border button__with-icon mt-4 mt-lg-6" title="Contact us">Contact Us<span class="button__icon"><svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <a href="#" class="button button__border button__with-icon mt-4 mt-lg-10" title="Contact us">Contact Us<span class="button__icon"><svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10 15.3879L17 8.38794L10 1.38794" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                     <path d="M17 8.38794L1 8.38794" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg> </span></a>
@@ -56,7 +56,9 @@ EnqueueAssetsJS:
                         </div>
                     </div>
                 </div>
-                <div class="col-4 d-none d-lg-flex"><img src="@asset('./images/hero-3.jpg')" alt="hero 3" class="hero-block__img hero-block__img--third"></div>
+                <div class="col-4 d-none d-lg-flex ">
+                    <div class="me-lg-6"><img src="@asset('./images/hero-3.jpg')" alt="hero 3" class="hero-block__img hero-block__img--third"></div>
+                </div>
             </div>
         </div>
     </div>
