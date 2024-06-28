acdf007a902c383a6c1c381a8fe747e9750741c2
Prakash Poudel <prakash.poudel@outside.tech>
2024-06-28 11:41:36 +0545

    Mobile footer refinement done

--- wp-content/themes/sage10/resources/views/sections/footer.blade.php
+++ wp-content/themes/sage10/resources/views/sections/footer.blade.php
@@ -1,3 +1,84 @@
-<footer class="content-info">
+<!-- <footer class="content-info">
   @php(dynamic_sidebar('sidebar-footer'))
+</footer> -->
+
+<footer class="footer bg-success ">
+    <div class="container text-white p-0">
+        <div class="py-10 px-6">
+            <div class="row row-cols-1 g-10">
+                <div class="col footer-top">
+                    <div class="footer-top__logo">
+                        <img src="@asset('./images/logo-white.svg')" alt="Logo in Footer">
+                    </div>
+                </div>
+                <div class="col footer-middle">
+                    <div class="footer-middle__navs">
+                        <div class="footer-middle__wrapper row row-cols-1 g-10">
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#">About us</a></p>
+                                <ul class="footer-middle-nav__links footer-middle-nav__links--desktop d-none">
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum dolor sit amet</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum dolor</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum dolor</a></li>
+                                </ul>
+                            </div>
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#">Our Team</a></p>
+                                <ul class="footer-middle-nav__links footer-middle-nav__links--desktop d-none">
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum dolor</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum dolor</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum dolor</a></li>
+                                </ul>
+                            </div>
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#">Who we are</a></p>
+                                <ul class="footer-middle-nav__links footer-middle-nav__links--desktop d-none">
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem ipsum</a></li>
+                                </ul>
+                            </div>
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#">Resources</a></p>
+                                <ul class="footer-middle-nav__links footer-middle-nav__links--desktop d-none">
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem Ist</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem </a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem Ist</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#">Lorem Ist</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col footer-bottom">
+                    <div class="footer-bottom__navs footer-bottom__navs--contact d-flex flex-column gap-10">
+                        <div class="footer-bottom-nav ">
+                            <p class="footer-bottom-nav__title h6"><a href="#">Contact</a></p>
+                            <ul class="footer-bottom-nav__links footer-bottom-nav__links--desktop d-none">
+                                <li class="footer-bottom-nav__link"><a href="#">Contact form</a></li>
+                                <li class="footer-bottom-nav__link"><a href="#">allowed@test.com</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-bottom-newsletter">
+                            <form action="" method="post" class="footer-bottom-newsletter__form d-flex flex-column gap-6">
+                                <label class="text-blush footer-bottom-newsletter__title fst-italic" for="newsletter__email">Sign up for our newsletter:</label>
+                                <input type="email" name="Email" id="newsletter__email" placeholder="Email address" class="footer-bottom-newsletter__input bg-transparent border border-2 border-white px-3 py-2" aria-required aria-label="Email Address"required>
+                                <button type="submit" class="button__border button__with-icon button__border--white" role="button">
+                                    Submit
+                                    <span class="button__icon">
+                                        <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10 15.3879L17 8.38794L10 1.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M17 8.38794L1 8.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                    </span>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="footer-bottom-nav footer-bottom-nav--empty"></div>
+                    </div>
+                </div>
+                <div class="col footer-copyright">Site content copyright © 2021 Allowded Text</div>
+            </div>
+        </div>
+    </div>
 </footer>
