0fe4368a750031ccbfa5d00f07d99a19534eeb6e
PNPout <prakash.poudel@outside.tech>
2024-06-28 15:12:38 +0545

    [OAD-230] Footer module refinement done
    * Mobile footer refinement done
    
    * Footer refinement for tab done
    
    * Footer done for desktop

--- wp-content/themes/sage10/resources/views/sections/footer.blade.php
+++ wp-content/themes/sage10/resources/views/sections/footer.blade.php
@@ -1,3 +1,84 @@
-<footer class="content-info">
+<!-- <footer class="content-info">
   @php(dynamic_sidebar('sidebar-footer'))
+</footer> -->
+
+<footer class="footer bg-success ">
+    <div class="container text-white p-0">
+        <div class="py-10 px-6 py-md-20 px-md-10 px-lg-20 pb-lg-40">
+            <div class="row row-cols-1 g-10 g-lg-20">
+                <div class="col footer-top">
+                    <div class="footer-top__logo">
+                        <img src="@asset('./images/logo-white.svg')" alt="Logo in Footer">
+                    </div>
+                </div>
+                <div class="col footer-middle">
+                    <div class="footer-middle__navs">
+                        <div class="footer-middle__wrapper row row-cols-1 row-cols-md-2 row-cols-lg-4 g-10">
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#" title="About us">About us</a></p>
+                                <ul class="footer-middle-nav__links list-unstyled footer-middle-nav__links--desktop d-none d-lg-flex flex-column gap-4 list-unstyled pt-6">
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum dolor sit amet</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum dolor</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum dolor</a></li>
+                                </ul>
+                            </div>
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#" title="Our team">Our Team</a></p>
+                                <ul class="footer-middle-nav__links list-unstyled footer-middle-nav__links--desktop d-none d-lg-flex flex-column gap-4 list-unstyled pt-6">
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum dolor</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum dolor</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum dolor</a></li>
+                                </ul>
+                            </div>
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#" title="Who we are">Who we are</a></p>
+                                <ul class="footer-middle-nav__links list-unstyled footer-middle-nav__links--desktop d-none d-lg-flex flex-column gap-4 list-unstyled pt-6">
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem ipsum</a></li>
+                                </ul>
+                            </div>
+                            <div class="col footer-middle-nav">
+                                <p class="footer-middle-nav__title h6"><a href="#" title="Resources">Resources</a></p>
+                                <ul class="footer-middle-nav__links footer-middle-nav__links--desktop d-none d-lg-flex flex-column gap-4 list-unstyled pt-6">
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem Ist</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem </a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem Ist</a></li>
+                                    <li class="footer-middle-nav__link"><a href="#" title="Lorem">Lorem Ist</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col footer-bottom">
+                    <div class="footer-bottom__navs footer-bottom__navs--contact d-flex flex-column gap-10 flex-lg-row row gap-lg-0 gx-lg-10">
+                        <div class="col-lg-3 footer-bottom-nav">
+                            <p class="footer-bottom-nav__title h6"><a href="#" title="Contact">Contact</a></p>
+                            <ul class="footer-bottom-nav__links footer-bottom-nav__links--desktop d-none d-lg-flex flex-column gap-4 list-unstyled pt-6">
+                                <li class="footer-bottom-nav__link"><a href="#" title="Contact Form">Contact form</a></li>
+                                <li class="footer-bottom-nav__link"><a href="mailto:allowed@test.com" title="Send email with your own email client">allowed@test.com</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 footer-bottom-newsletter pt-lg-7">
+                            <form action="" method="post" class="footer-bottom-newsletter__form d-flex flex-column flex-md-row flex-md-wrap gap-6 gap-md-4">
+                                <label class="footer-bottom-newsletter__title text-blush fst-italic" for="newsletter__email">Sign up for our newsletter:</label>
+                                <input type="email" name="Email" id="newsletter__email" placeholder="Email address" class="footer-bottom-newsletter__input bg-transparent border border-2 border-white px-3 py-2 flex-md-grow-1" aria-required aria-label="Email Address"required>
+                                <button type="submit" class="button__border button__with-icon button__border--white" role="button">
+                                    Submit
+                                    <span class="button__icon">
+                                        <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10 15.3879L17 8.38794L10 1.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M17 8.38794L1 8.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                    </span>
+                                </button>
+                            </form>
+                        </div>
+                        <!-- <div class="footer-bottom-nav footer-bottom-nav--empty"></div> -->
+                    </div>
+                </div>
+                <div class="footer-copyright text-blush">Site content copyright © 2021 Allowded Text</div>
+            </div>
+        </div>
+    </div>
 </footer>
