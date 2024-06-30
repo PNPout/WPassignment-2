54f95ed6147f3044301bd869695dd4b253707511
Prakash Poudel <prakash.poudel@outside.tech>
2024-06-30 16:51:38 +0545

    about us module refinement done

--- wp-content/themes/sage10/resources/views/blocks/about-us-b.blade.php
+++ wp-content/themes/sage10/resources/views/blocks/about-us-b.blade.php
@@ -28,29 +28,33 @@ $imgUrl = \Roots\asset('images/preview/leadspace.webp')->uri();
 <img loading="lazy" src="{!! $imgUrl !!}" style="width:100%;height:auto;">
 @else
 <section class="about-us-b">
-  <div class="container container-padding d-flex flex-column gap-10 flex-md-row">
-    <div class="about-content d-flex flex-column gap-10 flex-grow-1 ">
-      <p class="about-content__title h6 m-0">About Us</p>
-      <p class="about-content__text h3">
-        Integer at neque ac metus fringilla dapibus et eu eros. Vivamus
-        vitae finibus dolor. Suspendisse finibus ante in sapien fermentum,
-        quis condimentum turpis consequat.
-      </p>
-      <a href="#" class="about-content__button button button__fill button__with-icon">Learn more<span class="button__icon"><svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 15.3879L17 8.38794L10 1.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M17 8.38794L1 8.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          </svg> </span></a>
-    </div>
-    <div class="about-media bg-mint d-flex flex-column gap-4 p-6 flex-grow-1">
-      <p class="about-media__title h6 m-0"> How about Us?</p>
-      <div class="about-media__wrapper ">
-        <img loading="lazy" src="@asset('images/about-image.jpg')" style="width:100%;height:auto;" alt="Picture of Van in desert" class="about-media__image">
-        <!-- <img loading="lazy" src="<?php // echo get_template_directory_uri(); 
-                                      ?>/public/images/about-image.jpg"            " style="width:100%;height:auto;" alt="Picture of Van in desert"
-            class="about__image">  -->
-        <span class="about-media__play">
-          <img loading="lazy" src="@asset('images/play-button.svg')" style="width:100%;height:auto;" alt="play icon for about us video">
-        </span>
+  <div class="container container-padding ">
+    <div class="row g-10">
+      <div class="col-12 col-lg-6 about-content d-flex flex-column gap-10 justify-content-center">
+        <p class="about-content__title h6 m-0">About Us</p>
+        <p class="about-content__text h3">
+          Integer at neque ac metus fringilla dapibus et eu eros. Vivamus
+          vitae finibus dolor. Suspendisse finibus ante in sapien fermentum,
+          quis condimentum turpis consequat.
+        </p>
+        <a href="#" class="about-content__button button button__fill button__with-icon">Learn more<span class="button__icon"><svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M10 15.3879L17 8.38794L10 1.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M17 8.38794L1 8.38794" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg> </span></a>
+      </div>
+      <div class="col-12 col-lg-5 offset-lg-1 about-media ">
+        <div class="d-flex flex-column bg-mint  p-6 gap-4">
+          <p class="about-media__title h6 m-0">&nbsp; How about Us?</p>
+          <div class="about-media__wrapper ">
+            <img loading="lazy" src="@asset('images/about-image.jpg')" style="width:100%;height:auto;" alt="Picture of Van in desert" class="about-media__image">
+            <!-- <img loading="lazy" src="<?php // echo get_template_directory_uri();
+                                          ?>/public/images/about-image.jpg"            " style="width:100%;height:auto;" alt="Picture of Van in desert"
+                class="about__image">  -->
+            <span class="about-media__play">
+              <img loading="lazy" src="@asset('images/play-button.svg')" style="width:100%;height:auto;" alt="play icon for about us video">
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
