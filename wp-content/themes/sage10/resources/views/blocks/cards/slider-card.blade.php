5d5a0da630b7878c3394b8cf9e780b0fadd40616
Prakash Poudel <prakash.poudel@outside.tech>
2024-07-01 16:17:52 +0545

    slider refinement

--- wp-content/themes/sage10/resources/views/blocks/cards/slider-card.blade.php
+++ wp-content/themes/sage10/resources/views/blocks/cards/slider-card.blade.php
@@ -1,18 +1,19 @@
-
-          <div class="slider-card swiper-slide">
-            <div class="slider-card-media">
+          <div class="slider-cards__slide swiper-slide row d-flex g-0 gx-md-10 border-success">
+            <div class="slider-cards__media col-12 col-md-6">
               <img
                 src="{{ $imgScr }}"
                 alt="Image of a lady" 
-                class="slider-card-media__image"
+                class="slider-cards__image"
               />
             </div>
-            <div class="slider-card-content">
-              <h5 class="slider-card-content__title">
-               {{ $slideTitle}}
-              </h5>
-              <p class="slider-card-content__desc body-text__xl">
-              {{ $slideDesc}}
-              </p>
+            <div class="slider-cards__content col-12 col-md-6">
+              <div class="p-6 p-md-10 bg-white d-flex flex-column justify-content-center h-100 gap-10">
+                <p class="slider-cards__content-title h5 m-0">
+                 {{ $slideTitle}}
+                </p>
+                <p class="slider-cards__content-desc body-text__xl">
+                {{ $slideDesc}}
+                </p>
+              </div>
             </div>
           </div>
